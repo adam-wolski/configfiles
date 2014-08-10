9da76a5b885a49bf9cb0123c0d9eed75ae98e1a1
miniukov <uikslow@gmail.com>
2014-08-10 14:38:30 +0200

    Added modified draw mesh from gpencil

--- cg/Blender/MainCustomPlugin.py
+++ cg/Blender/MainCustomPlugin.py
@@ -1,7 +1,7 @@
 bl_info = {'name': 'Miniu tools',
         'description': 'Custom often used tools and scripts',
         'author': 'miniu',
-        'version': (0, 4, 2),
+        'version': (0, 4, 3),
         'blender': (2,71,0),
         'category': 'Mine',
         }
@@ -19,6 +19,8 @@ class MainCustomMenu(bpy.types.Menu):
         layout.operator('mine.disabledoublesidedforselected', text='Disable Double Side')
         layout.operator('mine.separateselectionwithoutdeleting', text='Separate Selection without deleting')
         layout.operator('mine.seamsfromsharpedges', text='Create seams from sharp')
+        layout.operator_context = 'INVOKE_DEFAULT'
+        layout.operator(DrawMesh.bl_idname, text='Draw Mesh', icon = "LINE_DATA")
 
 class OriginToSelectedScript(bpy.types.Operator):
     bl_idname = 'mine.origintoselectedscript'
@@ -113,6 +115,81 @@ class ToggleSubD(bpy.types.Operator):
 
         return {'FINISHED'}
 
+class DrawMesh(bpy.types.Operator):
+    """Copied and edited from booltool plugin"""
+    bl_idname = "mine.drawmesh"
+    bl_label = "Draw Mesh With Pencil"
+
+    count=0
+    
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    def modal(self,context, event):
+        self.count +=1
+        actObj = bpy.context.active_object
+        if self.count == 1:
+            actObj.select = True
+            bpy.ops.gpencil.draw('INVOKE_DEFAULT',mode ="DRAW")
+
+        if event.type in {'RET', 'NUMPAD_ENTER', 'E'}:
+            
+            bpy.ops.gpencil.convert(type='POLY')
+            for obj in context.selected_objects:
+                if obj.type == "CURVE":
+                    obj.name = "PolyDraw"
+                    bpy.context.scene.objects.active = obj
+                    bpy.ops.object.select_all(action='DESELECT')
+                    obj.select = True
+                    bpy.ops.object.convert(target="MESH")
+                    bpy.ops.object.mode_set(mode='EDIT')
+                    bpy.ops.mesh.select_all(action='SELECT')
+                    bpy.ops.mesh.edge_face_add()
+                    bpy.ops.mesh.flip_normals()
+                    bpy.ops.object.mode_set(mode='OBJECT')
+                    bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS')
+                    bpy.ops.object.modifier_add(type="SOLIDIFY")
+                    for mod in obj.modifiers:
+                        if mod.name == "Solidify":
+                            mod.name = "BTool_PolyBrush"
+                            mod.thickness = 1
+                            mod.offset = 0
+                            mod.edge_crease_outer = 1
+                            mod.edge_crease_inner = 1
+                    bpy.ops.object.shade_smooth()
+                    bpy.context.object.data.use_auto_smooth = True
+                    bpy.context.object.data.auto_smooth_angle = 0.872665
+
+                    obj["BoolToolPolyBrush"] = True             
+            
+                    bpy.ops.object.select_all(action='DESELECT')
+                    bpy.context.scene.objects.active = actObj
+                    bpy.context.scene.update()
+                    actObj.select = True
+                    obj.select = True
+                    actObj.grease_pencil.clear()
+                    bpy.ops.gpencil.data_unlink()
+                    
+            
+            return {'FINISHED'}
+
+        if event.type in {'ESC'}:
+            bpy.ops.ed.undo() # remove o Grease Pencil
+            return {'CANCELLED'}
+        
+        return {'RUNNING_MODAL'}
+
+
+    def invoke(self, context, event):
+        if context.object:
+            context.window_manager.modal_handler_add(self)
+            return {'RUNNING_MODAL'}
+        else:
+            self.report({'WARNING'}, "No active object, could not finish")
+            return {'CANCELLED'}
+        
+
 def register():
     bpy.utils.register_class(MainCustomMenu)
     bpy.utils.register_class(OriginToSelectedScript)
@@ -122,6 +199,7 @@ def register():
     bpy.utils.register_class(SeparateSelectionWithoutDeleting)
     bpy.utils.register_class(SeamsFromSharpEdges)
     bpy.utils.register_class(ToggleSubD)
+    bpy.utils.register_class(DrawMesh)
 
 def unregister():
     bpy.utils.unregister_class(MainCustomMenu)
@@ -132,6 +210,7 @@ def unregister():
     bpy.utils.unregister_class(SeparateSelectionWithoutDeleting)
     bpy.utils.unregister_class(SeamsFromSharpEdges)
     bpy.utils.unregister_class(ToggleSubD)
+    bpy.utils.unregister_class(DrawMesh)
 
 if __name__ == '__main__':
     register()
