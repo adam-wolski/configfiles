53d25c04b10cd5741cc72d5c02f25de44665bb9b
miniukov <uikslow@gmail.com>
2015-03-23 11:50:22 +0100

    blender config from work

---
+++ cg/Blender/2.74/scripts/presets/operator/export_scene.obj/lp100.py
@@ -0,0 +1,24 @@
+import bpy
+op = bpy.context.active_operator
+
+op.filepath = 'D:\\PVP\\wypelnienie_sciany_posag_throne_room\\obj\\catacombs_wall_l2_a_pedestal_a.obj'
+op.use_selection = True
+op.use_animation = False
+op.use_mesh_modifiers = True
+op.use_edges = True
+op.use_smooth_groups = False
+op.use_smooth_groups_bitflags = True
+op.use_normals = True
+op.use_uvs = True
+op.use_materials = True
+op.use_triangles = False
+op.use_nurbs = False
+op.use_vertex_groups = False
+op.use_blen_objects = True
+op.group_by_object = False
+op.group_by_material = False
+op.keep_vertex_order = True
+op.axis_forward = '-Z'
+op.axis_up = 'Y'
+op.global_scale = 100.0
+op.path_mode = 'AUTO'
