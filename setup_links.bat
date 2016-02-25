51b0a1d98a440837e7dd7b0bdfa42862040c86b6
miniukof <uikslow@gmail.com>
2016-02-25 16:40:55 +0100

    Delete old windows version of setup links

--- setup_links.bat
+++
@@ -1,4 +0,0 @@
-mklink C:\Users\awolski\_vimrc C:\git\configfiles\vim\vimrc
-mklink /D C:\Users\awolski\vimfiles\syntax C:\git\configfiles\vim\syntax
-mklink /D C:\Users\awolski\vimfiles\ftplugin C:\git\configfiles\vim\filetype
-mklink /D C:\Users\awolski\vimfiles\mysnippets C:\git\configfiles\vim\snippets
