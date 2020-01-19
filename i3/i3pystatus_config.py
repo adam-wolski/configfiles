51ee8206c2776f44772c566ba9e5da4e0e9d64d5
Adam Wolski <miniukof@gmail.com>
2020-01-19 09:43:03 +0100

    Change i3 status to py3status

--- i3/i3pystatus_config.py
+++ i3/i3pystatus_config.py
@@ -1,12 +1,14 @@
-from i3pystatus import Status
-from i3pystatus import IntervalModule
 from socket import gethostname 
 import re
 
-status = Status(standalone=True)
+from i3pystatus import Status
+from i3pystatus import IntervalModule
+
+STATUS = Status(standalone=True)
 
 
 class NetSpeed(IntervalModule):
+    """Display network usage"""
     interval = 1
 
     last_down = 0
@@ -42,18 +44,22 @@ class NetSpeed(IntervalModule):
                 u = u - self.tmp_upstart
 
             self.output = {
-                    "full_text": "{0}\\s {1}\\s  {2} {3}".format(
-                        self.bytes_print(dspeed),
-                        self.bytes_print(uspeed),
-                        self.bytes_print(d),
-                        self.bytes_print(u))
-                    }
+                "full_text": "{0}\\s {1}\\s  {2} {3}".format(
+                    self.bytes_print(dspeed),
+                    self.bytes_print(uspeed),
+                    self.bytes_print(d),
+                    self.bytes_print(u))
+                }
         else:
             self.output = {
-                    "full_text": "_"
-                    }
+                "full_text": "_"
+                }
 
     def toggle_tmp_measure(self):
+        """
+        Toggle temporary measure.
+        Where you start measuring usage from chossen moment.
+        """
         if self.tmp_measure:
             self.tmp_measure = False
         else:
@@ -121,102 +127,32 @@ class NetSpeed(IntervalModule):
             interval = 1
 
 
-# Displays clock like this:
-# Tue 30 Jul 11:59:46 PM KW31
-#                          ^-- calendar week
-status.register("clock",
-    format="%a %-d %b %X",)
+STATUS.register("clock", format="%a %-d %b %X",)
 
-# Shows the average load of the last minute and the last 5 minutes
-# (the default value for format is used)
-# status.register("load")
+STATUS.register("load")
 
-# Shows your CPU temperature, if you have a Intel CPU
-# status.register("temp",
-#     format="{temp:.0f}°C",)
-
-# The battery monitor has many formatting options, see README for details
-
-# This would look like this, when discharging (or charging)
-# ↓14.22W 56.15% [77.81%] 2h:41m
-# And like this if full:
-# =14.22W 100.0% [91.21%]
-#
-# This would also display a desktop notification (via D-Bus) if the percentage
-# goes below 5 percent while discharging. The block will also color RED.
-# If you don't have a desktop notification demon yet, take a look at dunst:
-#   http://www.knopwob.org/dunst/
 if gethostname() == 'miniucer':
-    status.register("battery",
-        # format="{status}/{consumption:.2f}W {percentage:.2f}% [{percentage_design:.2f}%] {remaining:%E%hh:%Mm}",
-        format="{status}/{consumption:.2f}W {percentage:.2f}% {remaining:%E%hh:%Mm}",
-        alert=True,
-        alert_percentage=5,
-        status={
+    BATTERY = {
+        "format": "{status}/{consumption:.2f}W {percentage:.2f}% {remaining:%E%hh:%Mm}",
+        "alert": True,
+        "alert_percentage": 10,
+        "status": {
             "DIS": "↓",
             "CHR": "↑",
             "FULL": "=",
-        },)
+        },
+    }
+    STATUS.register("battery", BATTERY)
 
-# This would look like this:
-# Discharging 6h:51m
-#status.register("battery",
-#    format="{status} {remaining:%E%hh:%Mm}",
-#    alert=True,
-#    alert_percentage=5,
-#    status={
-#        "DIS":  "Discharging",
-#        "CHR":  "Charging",
-#        "FULL": "Bat full",
-#    },)
 
-# Displays whether a DHCP client is running
-#status.register("runwatch",
-#    name="DHCP",
-#    path="/var/run/dhclient*.pid",)
-
-# Shows the address and up/down state of eth0. If it is up the address is shown in
-# green (the default value of color_up) and the CIDR-address is shown
-# (i.e. 10.10.10.42/24).
-# If it's down just the interface name (eth0) will be displayed in red
-# (defaults of format_down and color_down)
-#
-# Note: the network module requires PyPI package netifaces
-#status.register("network",
-#    interface="eth0",
-#    format_up="{v4cidr}",)
-
-# Note: requires both netifaces and basiciw (for essid and quality)
-#status.register("network",
-#    interface="wlan0",
-#    format_up="{essid} {quality:03.0f}%",)
-
-# Shows disk usage of /
-# Format:
-# 42/128G [86G]
-#status.register("disk",
-#    path="/",
-#    format="{used}/{total}G [{avail}G]",)
-
-# Shows pulseaudio default sink volume
-#
-# Note: requires libpulseaudio from PyPI
-status.register("pulseaudio",
-    format="♪{volume}",
-    )
-
-# Shows mpd status
-# Format:
-# Cloud connected▶Reroute to Remain
-#status.register("mpd",
-#    format="{title}{status}{album}",
-#    status={
-#        "pause": "▷",
-#        "play": "▶",
-#        "stop": "◾",
-#    },)
+STATUS.register("pulseaudio", format="♪{volume}")
 
 if gethostname() == 'miniucer':
-    status.register(NetSpeed)
+    STATUS.register(NetSpeed)
 
-status.run()
+SPOTIFY = {
+    "format": "{title} by {artist}",
+}
+STATUS.register("spotify", SPOTIFY)
+
+STATUS.run()
